4944505398a5c346cd46b34c7524db348fed8253
kaitlynmalsky <93287571+kaitlynmalsky@users.noreply.github.com>
2024-11-04 23:33:43 -0500

    Adding tags now updates database. Tested with localhost

--- ClosetLab/Calendar.js
+++ ClosetLab/Calendar.js
@@ -2,10 +2,17 @@ import { SafeAreaView } from "react-native-web"
 import { Text } from "react-native"
 import styles from "./Stylesheet"
 
+
+const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 export function CalendarView() {
+
+    const today = new Date()
+    const cur_month = month[today.getMonth()]
     return (
         <SafeAreaView style={styles.container}>
             <Text style={styles.title}>Hello, [USER]!</Text>
+            <Text style={styles.text}>{cur_month}</Text>
         </SafeAreaView>
     )
 }
