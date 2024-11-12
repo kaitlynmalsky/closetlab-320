ea455799e28292d98c876788f8315f6eb8608da3
emirkaan5 <emirkorukluoglu@gmail.com>
2024-11-12 15:19:20 -0500

    test

--- ClosetLab/Backend/app.py
+++ ClosetLab/Backend/app.py
@@ -151,4 +151,4 @@ def delete_outfit(outfit_id):
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=5000)
